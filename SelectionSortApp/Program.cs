db8e06bcff10de1903bd0b122bb657b4d0d72ebf
Vpetros <rawkudodes@hotmail.com>
2025-09-09 21:46:45 +0300

    feat: Selection Sort App

---
+++ SelectionSortApp/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SelectionSortApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 11, 2, 33, 14, 5, 68, 7, 8, 29 };
+            int minPosition;
+            int min;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                minPosition = i;
+                min = arr[minPosition];
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < min)
+                    {
+                        minPosition = j;
+                        min = arr[j];
+                    }
+                }
+
+                Swap(arr, i, minPosition);
+            }
+            
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item + " ");
+            }
+        }
+
+        public static void Swap(int[] arr, int i, int j)
+        {
+            (arr[j], arr[i]) = (arr[i], arr[j]);
+        }
+    }
+}
