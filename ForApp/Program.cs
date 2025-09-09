b526695b57efdf00db8d5c7bc90bd6cbe41b1f4a
Vpetros <rawkudodes@hotmail.com>
2025-09-09 21:43:57 +0300

    feat: For App

---
+++ ForApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ForApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine($"Άθροισμα: {sum}");
+
+            sum = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0) continue;
+                sum += i;
+            }
+
+            Console.WriteLine($"Άθροισμα Μονών: {sum}");
+
+        }
+    }
+}
