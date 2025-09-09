82234c4db60cccf88fadbfe0f02c25b8485293b8
Vpetros <rawkudodes@hotmail.com>
2025-09-09 21:45:33 +0300

    feat: Read App

---
+++ ReadApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ReadApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float f = 0.0f;
+            double d = 0d;
+
+            Console.WriteLine("Insert 2 decimal");
+            f = float.Parse(Console.ReadLine()!);
+            d = double.Parse(Console.ReadLine()!);
+
+            Console.WriteLine($"f = {f:N2}\td = {d:N2}");
+        }
+    }
+}
