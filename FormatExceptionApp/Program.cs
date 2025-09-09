25ff07c290f51074099d8a81a4e21096ae78f902
Vpetros <rawkudodes@hotmail.com>
2025-09-09 21:44:20 +0300

    feat: Format Exception App

---
+++ FormatExceptionApp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FormatExceptionApp
+{
+    /// <summary>
+    /// Διαβάζει ένα string είσοδο και προσπαθεί 
+    /// να μετατρέψει σε int και ελέγχει με try/catch
+    /// για FormatException
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Παρακαλώ εισάγετε έναν ακέραιο (0 για έξοδο)");
+                    num = int.Parse(Console.ReadLine()!);
+                    Console.WriteLine($"Εισάγατε τον ακέραιο: {num}");
+                    if (num == 0) break;
+                    
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            }
+        }
+    }
+
+
