297b4b026bac9cd277fa83f66cc1b69a6c856277
Vpetros <rawkudodes@hotmail.com>
2025-09-09 21:46:31 +0300

    feat: Safe Read App

---
+++ SafeReadApp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SafeReadApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal d1 = 0m;
+            decimal d2 = 0m;
+
+            Console.WriteLine("Insert 2 decimal");
+
+            if (!decimal.TryParse(Console.ReadLine(), out d1))
+            {
+                Console.WriteLine("Error in input");
+                return;
+
+            }
+
+            if (!decimal.TryParse(Console.ReadLine(), out d2))
+            {
+                Console.WriteLine("Error in input");
+                return;
+
+            }
+
+            Console.WriteLine($"d1: {d1}, d2: {d2}");
+        }
+    }
+}
