44aba7f826762f53c24873d4086f5afe35ee9808
Vpetros <rawkudodes@hotmail.com>
2025-09-09 21:42:17 +0300

    feat: Enchanced For App

---
+++ EnchancedForApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EnchancedForApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "Athens", "London", "Paris" };
+
+            foreach (string city in cities)
+            {
+                Console.WriteLine(city + " ");
+            }
+        }
+    }
+}
