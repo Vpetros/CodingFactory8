97ac8d7526d5c0b88e8e00acb5697db6702c5d79
Vpetros <rawkudodes@hotmail.com>
2025-09-10 20:05:22 +0300

    feat: Nullable Str

---
+++ NullableStr/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Channels;
+
+namespace NullableStr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? s1 = null;         // non-nullable warning
+            string? s2 = null;          // nullable
+
+            s1 = Console.ReadLine();    // non-nullable warning
+            s2 = Console.ReadLine();    // nullable
+
+            if ((s1 != null) && s1.Equals("Coding") && (s2 != null) && s2.Equals("Coding"))
+            {
+                Console.WriteLine("Bingo");
+            }
+
+
+        }
+
+        public static string? GetUpperString (string? s)
+        {
+            return s?.ToUpper();        // null-Condition Operator
+        }
+
+        public static string GetUpperString2(string? s)
+        {
+            return s?.ToUpper() ?? "NULL";          //Coalescing Operator
+        }
+    }
+}
