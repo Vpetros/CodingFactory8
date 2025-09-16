921642e54e97b524676364d93e799c3e4ed096a5
Vpetros <rawkudodes@hotmail.com>
2025-09-16 21:09:38 +0300

    feat: Shallow Deep Copy App

---
+++ ShallowDeepCopyApp/Model/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShallowDeepCopyApp.Model
+{
+    internal class Address
+    {
+        public string? Street { get; set; }
+        public String? Number { get; set; }
+        public String? ZipCode { get; set; }
+
+    }
+}
